02d6543cbaf103ba17b01034e3534ba899567ef0
thetryworks <natesmyth1@gmail.com>
2022-07-08 14:46:49 -0500

    added activater event listener to delete cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -45,3 +45,27 @@ self.addEventListener('install', function (e) {
         })
     )
 })
+
+// Delete outdated caches
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        // `keyList` contains all cache names under your username.github.io
+        // filter out ones that has this app prefix to create keeplist
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        // add current cache name to keeplist
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+  });
