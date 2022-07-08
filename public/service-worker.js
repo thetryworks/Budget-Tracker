d0da49ed4a99c68d605edaf746c9ef3fcfbd1cc7
thetryworks <natesmyth1@gmail.com>
2022-07-08 14:45:52 -0500

    added fetch cache event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,6 +18,30 @@ const FILES_TO_CACHE = [
     './icons/icon-72x72.png'
 ];
 
-self.addEventListener('install', function (e) {
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
 
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
 })
